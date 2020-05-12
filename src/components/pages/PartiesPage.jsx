e3d9fbdd1e98061368f37bb7078b176892e5667c
Sergey Vlasenko <sergey_vlasenko@relex.ru>
2020-05-12 11:43:36 +0300

    3

--- src/components/pages/PartiesPage.jsx
+++ src/components/pages/PartiesPage.jsx
@@ -2,6 +2,67 @@ import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchParties } from "../../features/parties/partiesSlice";
+import styled from "styled-components";
+
+const StyledCreatePartyLink = styled(Link)`
+  color: #494949 !important;
+  text-transform: uppercase;
+  text-decoration: none;
+  background: #ffffff;
+  padding: 20px;
+  border: 4px solid #494949 !important;
+  display: inline-block;
+  transition: all 0.4s ease 0s;
+`;
+
+const StyledCreatePartyWrapper = styled.div`
+  text-align: center;
+  margin-top: 20px;
+  margin-bottom: 20px;
+`;
+
+const StyledPartyItems = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+
+const StyledPartyItem = styled(Link)`
+  margin-bottom: 30px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  border-radius: 5px;
+  padding: 20px;
+  width: 45%;
+
+  :hover {
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+  }
+`;
+
+const StyledPartyItemName = styled.div`
+  font-size: 18px;
+  line-height: 22px;
+  text-align: center;
+  margin-bottom: 20px;
+`;
+
+const StyledPartyItemInfo = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const StyledPartyItemInfoContent = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 18px;
+  line-height: 22px;
+`;
+
+const StyledPartyItemInfoImage = styled.img`
+  margin-right: 15px;
+`;
 
 const PartiesPage = () => {
   const dispatch = useDispatch();
@@ -14,15 +75,29 @@ const PartiesPage = () => {
   return (
     <div>
       <h1>Готовые идеи</h1>
-      <Link to="/parties/create">Создать</Link>
-      <div>
+      <StyledCreatePartyWrapper>
+        <StyledCreatePartyLink to="/parties/create">
+          Создать подборку
+        </StyledCreatePartyLink>
+      </StyledCreatePartyWrapper>
+      <StyledPartyItems>
         {parties &&
           parties.map((party) => (
-            <div>
-              <Link to={`/parties/${party.id}`}>{party.name}</Link>
-            </div>
+            <StyledPartyItem to={`/parties/${party.id}`}>
+              <StyledPartyItemName>{party.name}</StyledPartyItemName>
+              <StyledPartyItemInfo>
+                <StyledPartyItemInfoContent>
+                  <StyledPartyItemInfoImage src="/guests.png" alt="" />{" "}
+                  {party.guestsCount} гостей
+                </StyledPartyItemInfoContent>
+                <StyledPartyItemInfoContent>
+                  <StyledPartyItemInfoImage src="/glass.png" alt="" />{" "}
+                  {party.cocktailsCount} коктейля
+                </StyledPartyItemInfoContent>
+              </StyledPartyItemInfo>
+            </StyledPartyItem>
           ))}
-      </div>
+      </StyledPartyItems>
     </div>
   );
 };
--- src/components/pages/PartyPage.jsx
+++ src/components/pages/PartyPage.jsx
@@ -2,13 +2,66 @@ import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchParty } from "../../features/parties/partySlice";
-import Cocktail from "../cocktails/Cocktail";
-import CocktailesList from "../cocktails/CocktailesList";
+import styled from "styled-components";
+
+const StyledPartyGuestsInfo = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 30px;
+`;
+
+const StyledPartyGuestsInfoImage = styled.img`
+  margin-right: 15px;
+`;
+
+const StyledPartyGuestsInfoText = styled.span`
+  font-size: 18px;
+  line-height: 22px;
+`;
+
+const StyledPartyInfo = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 10px;
+`;
+
+const StyledH2 = styled.h2`
+  text-transform: none;
+  text-align: left;
+  margin-bottom: 20px;
+`;
+
+const StyledMainInfo = styled.div`
+  display: flex;
+`;
+
+const StyledMainInfoIngredients = styled.div`
+  width: calc(55% - 60px);
+  flex-shrink: 0;
+  margin-right: 60px;
+`;
+
+const StyledIngredientWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+`;
+
+const StyledIngredientDots = styled.div`
+  border-bottom: 2px dotted #757575;
+  flex-grow: 1;
+  margin: 0 10px 5px 5px;
+`;
+
+const StyledMainInfoContent = styled.div`
+  padding-left: 30px;
+  text-align: justify;
+`;
 
 const PartyPage = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
-  const party = useSelector((state) =>  state.party ? state.party.item : null);
+  const party = useSelector((state) => (state.party ? state.party.item : null));
 
   useEffect(() => {
     if (id) {
@@ -23,8 +76,20 @@ const PartyPage = () => {
   return (
     <div>
       <h1>{party.name}</h1>
-      <div>{party.guestsCount}</div>
-      <div>{party.cocktailAmount.length}</div>
+      <StyledPartyInfo>
+        <StyledPartyGuestsInfo>
+          <StyledPartyGuestsInfoImage src="/guests.png" alt="" />
+          <StyledPartyGuestsInfoText>
+            {party.guestsCount} гостей
+          </StyledPartyGuestsInfoText>
+        </StyledPartyGuestsInfo>
+        <StyledPartyGuestsInfo>
+          <StyledPartyGuestsInfoImage src="/glass.png" alt="" />
+          <StyledPartyGuestsInfoText>
+            {party.cocktailAmount.length} гостей
+          </StyledPartyGuestsInfoText>
+        </StyledPartyGuestsInfo>
+      </StyledPartyInfo>
       <div>
         {party.cocktailAmount.map((cocktailAmount) => (
           <div>
@@ -32,15 +97,26 @@ const PartyPage = () => {
           </div>
         ))}
       </div>
-      <div>
-        {party.ingredientAmount.map((ingredientAmount) => (
-          <div>
-            <div>{ingredientAmount.ingredient.nameRu}</div>{" "}
-            <div>{ingredientAmount.amount}</div>
-          </div>
-        ))}
-      </div>
-      <div>{party.legend}</div>
+      <StyledMainInfo>
+        <StyledMainInfoIngredients>
+          <StyledH2>Вам понадобится:</StyledH2>
+          <StyledMainInfoContent>
+            {party.ingredientAmount.map((ingredientAmount) => (
+              <StyledIngredientWrapper>
+                <div>{ingredientAmount.ingredient.nameRu}</div>
+                <StyledIngredientDots />
+                <div>{ingredientAmount.amount}</div>
+              </StyledIngredientWrapper>
+            ))}
+          </StyledMainInfoContent>
+        </StyledMainInfoIngredients>
+        <div>
+          <StyledH2>Описание:</StyledH2>
+          <StyledMainInfoContent>
+            <div>{party.legend}</div>
+          </StyledMainInfoContent>
+        </div>
+      </StyledMainInfo>
     </div>
   );
 };
