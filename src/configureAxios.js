1e0bac26b4d337f6a2ac8a65c18e7b4043e42bb4
Sergey Vlasenko <sergey_vlasenko@relex.ru>
2020-05-25 18:39:08 +0300

    Change axios config

--- src/configureAxios.js
+++ src/configureAxios.js
@@ -2,6 +2,9 @@ import axios from "axios";
 import qs from "qs";
 
 const configureAxios = () => {
+  axios.defaults.baseURL =
+    process.env.NODE_ENV === "production" ? `http://${window.location.hostname}:8080/` : "";
+
   axios.interceptors.response.use(
     (response) =>
       new Promise((resolve) => setTimeout(() => resolve(response.data), 500)),
